f3e12f22a58519f6116f080c85bfd2168c145c39
yashodhanAgale <yashodhanagale3@gmail.com>
2025-09-17 22:30:37 +0530

    URL changes for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7777",
+  baseURL: "https://real-time-polling-backend-pvy8.onrender.com",
   withCredentials: true,
 });
 
